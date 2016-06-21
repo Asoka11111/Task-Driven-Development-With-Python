2adf8b23b063ae71398af799bdbd45e62a645425
Ugonna Erinne <ue24@cornell.edu>
2016-06-21 11:17:42 -0500

    Another shot at the Procfile

--- superlists/urls.py
+++ superlists/urls.py
@@ -23,6 +23,7 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', list_views.home_page, name='home'),
+    url(r'^https://tdd-akosa.herokuapp.com/$', list_views.home_page)
     url(r'^lists/', include(list_urls)),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url('', include('django.contrib.auth.urls', namespace='auth')),
